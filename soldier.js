b3a0657efa0139674b5559d1f637c546abe39c77
Begaev <ip.huligan@gmail.com>
2022-06-07 17:53:51 +0300

    subtask 1

---
+++ soldier.js
@@ -0,0 +1,11 @@
+const soldier = {
+    name: 'Andrey',
+    health: 10,
+    weapon: {
+       name: ak47,
+       bullets: 30,
+    supplies: 3,
+    shoot: function () {},
+    recharge: function () {},
+    hurt: function () {},
+}
