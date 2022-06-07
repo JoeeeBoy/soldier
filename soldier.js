7f4c832c99c15d171ce7236d8a93ae18cf1a9193
Begaev <ip.huligan@gmail.com>
2022-06-07 18:22:41 +0300

    уже могут стрелять))

--- soldier.js
+++ soldier.js
@@ -1,11 +1,31 @@
 const soldier = {
-    name: 'Andrey',
-    health: 10,
-    weapon: {
-       name: ak47,
-       bullets: 30,
-    supplies: 3,
-    shoot: function () {},
-    recharge: function () {},
-    hurt: function () {},
-}
+  name: "Andrey",
+  health: 10,
+  weapon: {
+    name: "ak47",
+    bullets: 30,
+  },
+  supplies: 3,
+  shoot: function () {
+    if (this.weapon.bullets < 1) {
+      console.log("обойма пуста. Перезаредитесь");
+    }
+    this.weapon.bullets--;
+    console.log("бах-бах");
+  },
+  recharge: function () {
+    if (this.supplies < 1) {
+      console.log("обойма пуста. Перезаредитесь");
+    }
+    this.weapon.bullets = 30;
+    this.supplies--;
+    console.log("перезарядка...");
+  },
+  wound: function () {
+    if (this.health < 1) {
+      console.log("Ты проиграл");
+    }
+    this.health--;
+    console.log("ранен");
+  },
+};
